453e90859dc997b458816299b8c93efab97c1863
Rick Toews <rick.toews@gmail.com>
2021-01-11 11:52:43 -0800

    Add About page.

---
+++ src/About.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+function About(props) {
+
+	return (
+	<div className="plain-content container">
+	  <h3>About</h3>
+	  <p>I've been a lover of words since I was a boy, and I was once passionate about enlarging my vocabulary. At some point, my interest in words that were merely among those an educated person ought to know gave way to a fascination for words rather more off the beaten path; and when I discovered Mrs. Byrne's Dictionary of Unusual, Obscure, and Preposterous Words, I thought I'd died and gone to heaven.</p>
+
+	  <p>This is not s resource intended as an almost academic aid in pragmatically vocabulary building. These are not words you learn in order to sound educated--though if you were to use them in ordinary conversation, you might come off a touch pretentious. No: these are words you learn for no reason other than that you're drawn to cool and unusual words.</p>
+
+	  <h4>Sources</h4>
+
+	  <p>Most of the words included here were taken, as far I can tell, from an online resource titled Luciferous Logolepsy, which no longer exists. Years ago--at least a decade, I should think--I created a database table of words and populated it with roughly 9,000 worthy specimens. I don't recall where I got the words, or how; however, when I compare entries with those at the Cafe Arcane (https://arcane.org/luciferous-logolepsy/), I find the definitions are identical, and certain misspellings are the same. I infer a common source.</p>
+
+	  <p>Since what I believe to have been my original source no longer exists, I feel more free than I otherwise might to use the list for my purposes here.</p>
+
+	  <p>Another longtime trove for the logophile is The Phrontistery (phrontistery.info). From this, I took the 412 entries within the group titled Compendium of Lost Words.</p>
+	</div>
+	);
+}	
+
+export default About;
