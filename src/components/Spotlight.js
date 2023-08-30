492a3f2310df975094e89ddf1768265e68018b35
Rick Toews <rick.toews@gmail.com>
2023-08-29 22:16:47 -0700

    Add message to empty Spotlight page. The massive changes to App.scss were mostly formatting from VS Code. The change of importance is adding .spotlight-container to the list of other containers whose descendants include .browse. It's currently around line 412.

--- src/components/Spotlight.js
+++ src/components/Spotlight.js
@@ -5,60 +5,70 @@ import WordsInterface from '../utils/words-interface';
 import Scramble from './Scramble';
 
 function Spotlight(props) {
-	// Check state for selected scrambled word.
-	const state = props.location.state;
-	// Check for word to be scrambled, passed in with URL.
-	const spotlightWord = props.match?.params?.word;
-	const [randomPick, setRandomPick] = useState(!spotlightWord && state === undefined ? true : false);
-	var scrambledItem;
-	if (randomPick === false) {
-		// Not random if word was passed via URL or state.
-		if (spotlightWord) {
-			scrambledItem = WordsInterface.getWordObjByWord(spotlightWord);
-		} else if (state) {
-			scrambledItem = state.wordObj;
-		} else {
-			scrambledItem = WordsInterface.getSpotlightItem();
-		}
-	}
-	else {
-		scrambledItem = WordsInterface.getSpotlightItem();
-	}
-	const [item, setItem] = useState(scrambledItem);
+    const [introMessage, setIntroMessage] = useState('');
+    const [randomeWordSelected, setRandomWordSelected] = useState(false);
 
-	useEffect(() => {
-		if (props.match.params.word && props.match.params.def) {
-			let { word, def } = props.match.params;
-			WordsInterface.saveCustomWord(-1, word, def);
-			props.history.push('/spotlight-list');
-		} else if (item.word === '') {
-			props.history.push('/browse');
-		}
-	}, []);
+    // Check state for selected scrambled word.
+    const state = props.location.state;
+    // Check for word to be scrambled, passed in with URL.
+    const spotlightWord = props.match?.params?.word;
+    const [randomPick, setRandomPick] = useState(!spotlightWord && state === undefined ? true : false);
+    var scrambledItem;
+    if (randomPick === false) {
+        // Not random if word was passed via URL or state.
+        if (spotlightWord) {
+            scrambledItem = WordsInterface.getWordObjByWord(spotlightWord);
+        } else if (state) {
+            scrambledItem = state.wordObj;
+        } else {
+            scrambledItem = WordsInterface.getSpotlightItem();
+        }
+    }
+    else {
+        scrambledItem = WordsInterface.getSpotlightItem();
+    }
+    const [item, setItem] = useState(scrambledItem);
 
-	const handleAnother = e => {
-		var anotherItem = WordsInterface.getSpotlightItem();
-		setItem(anotherItem);
-	};
+    useEffect(() => {
+        if (props.match.params.word && props.match.params.def) {
+            let { word, def } = props.match.params;
+            WordsInterface.saveCustomWord(-1, word, def);
+            props.history.push('/spotlight-list');
+        } else if (item.word === '') {
+            setIntroMessage(`Once you've marked words you want to learn, come here for the challenge of unscrambling random selections.`)
+            //props.history.push('/browse');
+        }
+    }, []);
 
-	return (
-	<div className="spotlight-container">
-	  <div className="spotlight-wrapper">
-	    <div className="spotlight">
-		  <div className="word-item-def-container">
-	        <div aria-label="def" className="word-item-def">
-	          {item.def}
-	        </div>
-		  </div>
-	      <Scramble item={item} word={item.word} />
+    const handleAnother = e => {
+        var anotherItem = WordsInterface.getSpotlightItem();
+        setItem(anotherItem);
+    };
 
-	    </div>
-	    <div className="button-wrapper">
-	      <button className={'badge badge-another'} onClick={handleAnother}><i className="glyphicon glyphicon-play"></i> Another</button>
-	    </div>
-	  </div>
-	</div>
-	);
+    return (
+        <div className="spotlight-container">
+            {item.word > '' ? (
+                <div className="spotlight-wrapper">
+                    <div className="spotlight">
+                        <div className="word-item-def-container">
+                            <div aria-label="def" className="word-item-def">
+                                {item.def}
+                            </div>
+                        </div>
+                        <Scramble item={item} word={item.word} />
+
+                    </div>
+                    <div className="button-wrapper">
+                        <button className={'badge badge-another'} onClick={handleAnother}><i className="glyphicon glyphicon-play"></i> Another</button>
+                    </div>
+                </div>)
+                : (
+                    <div className="browse">
+                        <p>{introMessage}</p>
+                    </div>
+                )}
+        </div>
+    );
 }
 
 export default withRouter(Spotlight);
