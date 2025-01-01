beee0732ff82cfefff4ebe9e5afbb951891db40d
Rick Toews <rick.toews@gmail.com>
2024-12-31 18:07:26 -0800

    Update X icon.

--- src/components/WordEntry.js
+++ src/components/WordEntry.js
@@ -1,7 +1,7 @@
 import WordsInterface from '../utils/words-interface';
 import WordEntryButtons from './WordEntryButtons';
-import {FacebookShareButton, FacebookIcon} from "react-share";
-import {TwitterShareButton, TwitterIcon} from "react-share";
+import { FacebookShareButton, FacebookIcon } from "react-share";
+import { TwitterShareButton, XIcon } from "react-share";
 
 function WordEntry(props) {
 	const { history } = props;
@@ -18,7 +18,7 @@ function WordEntry(props) {
 			wordEl.classList.remove('deleted');
 			WordsInterface.undeleteCustomWord(wordObj);
 		}
-	
+
 	}
 
 	function editWord(wordObj, e) {
@@ -34,10 +34,10 @@ function WordEntry(props) {
 
 	function FacebookShare(wordObj) {
 		return (
-			<FacebookShareButton 
-			url={'https://wordmage.app/spotlight/' + wordObj.word}
-			quote={`${wordObj.word.toUpperCase()}. ${wordObj.def}`}
-			className={'share-btn'}>
+			<FacebookShareButton
+				url={'https://wordmage.app/spotlight/' + wordObj.word}
+				quote={`${wordObj.word.toUpperCase()}. ${wordObj.def}`}
+				className={'share-btn'}>
 				<FacebookIcon size={32} round={true} />
 			</FacebookShareButton>
 		);
@@ -46,34 +46,34 @@ function WordEntry(props) {
 
 	function TwitterShare(wordObj) {
 		return (
-			<TwitterShareButton 
-			url={'https://wordmage.app/spotlight/' + wordObj.word}
-			title={`${wordObj.word.toUpperCase()}. ${wordObj.def}`}
-			hashtags={['wordmage']}
-			className={'share-btn'}>
-				<TwitterIcon size={32} round={true} />
+			<TwitterShareButton
+				url={'https://wordmage.app/spotlight/' + wordObj.word}
+				title={`${wordObj.word.toUpperCase()}. ${wordObj.def}`}
+				hashtags={['wordmage']}
+				className={'share-btn'}>
+				<XIcon size={32} round={true} />
 			</TwitterShareButton>
 		);
 	}
 
 	return wordObj.divider ? <hr className="rejects" /> : (
-	  <div className="word-item">
-	    <div className="word-item-word-container">
-	      <div className="word-item-word">{wordObj.word}</div>
-		    <div className="scramble-btn" onClick={() => { scrambleWord(wordObj); }}><i className="glyphicon glyphicon-retweet"></i></div>
-	      { wordObj.myown ? <div className="trash-btn" onClick={e => { deleteWordToggle(wordObj, e); }}><i className="glyphicon glyphicon-trash"></i></div> : null }
-	      { wordObj.myown ? <div className="edit-btn" onClick={e => { editWord(wordObj, e); }}><i className="glyphicon glyphicon-pencil"></i></div> : null }
-        { FacebookShare(wordObj) }
-        { TwitterShare(wordObj) }
-	    </div>
-	    <div className="word-item-def-container">
-	      <div className="source-def-container">
-	        <div className="word-item-def">{wordObj.def}</div>
-		<span className="word-item-source">(source: {wordObj.source})</span>
-	      </div>
-	      <WordEntryButtons wordObj={wordObj} listType={listType} popupTags={props.popupTags} />
-	    </div>
-	  </div>
+		<div className="word-item">
+			<div className="word-item-word-container">
+				<div className="word-item-word">{wordObj.word}</div>
+				<div className="scramble-btn" onClick={() => { scrambleWord(wordObj); }}><i className="glyphicon glyphicon-retweet"></i></div>
+				{wordObj.myown ? <div className="trash-btn" onClick={e => { deleteWordToggle(wordObj, e); }}><i className="glyphicon glyphicon-trash"></i></div> : null}
+				{wordObj.myown ? <div className="edit-btn" onClick={e => { editWord(wordObj, e); }}><i className="glyphicon glyphicon-pencil"></i></div> : null}
+				{FacebookShare(wordObj)}
+				{TwitterShare(wordObj)}
+			</div>
+			<div className="word-item-def-container">
+				<div className="source-def-container">
+					<div className="word-item-def">{wordObj.def}</div>
+					<span className="word-item-source">(source: {wordObj.source})</span>
+				</div>
+				<WordEntryButtons wordObj={wordObj} listType={listType} popupTags={props.popupTags} />
+			</div>
+		</div>
 	);
 }
 
