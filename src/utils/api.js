cab6b21281b3141968fb5982f387c4196e462b02
Rick Toews <rick.toews@gmail.com>
2026-06-07 16:35:09 -0700

    Remove legacy sharing.

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,31 +3,6 @@ import { authFetch } from './auth';
 
 const base = 'https://wordmage.toews-api.com';
 
-const urls = {
-	share: base + '/words.php/sendlocal',
-	receive: base + '/words.php/retrievelocal/'
-};
-
-async function share(data) {
-	const response = await authFetch(urls.share, {
-		method: 'POST',
-		//		mode: 'no-cors',
-		//		cache: 'no-cache',
-		//		credentials: 'omit',
-		//		referrerPolicy: 'no-referrer-when-downgrade',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(data)
-	});
-	return response.json();
-}
-
-async function receive(code) {
-	const response = await authFetch(urls.receive + code);
-	return response.json();
-}
-
 async function getWordsPage(params = {}) {
 	const { starts_with = 'a', after_word, limit = 200 } = params;
 	const queryParams = new URLSearchParams();
@@ -54,4 +29,4 @@ async function getRandomPageData(user_id) {
 }
 
 
-export { share, receive, getWordsPage, getRandomPageData };
+export { getWordsPage, getRandomPageData };
